596df3ce3c4dc264c28b99fe6129eb86aa81a8ee
ayanfeOlugbeja <ayanfeolugbeja@gmail.com>
2024-06-21 13:32:40 -0700

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,45 +1,65 @@
-import Linked from './../components/Linked';
-import me from './../images/me.png';
-import react from './../images/react.svg';
-import html from './../images/html.svg';
-import css from './../images/css.svg';
-import javaScript from './../images/javaScript.svg';
-import firebase from './../images/firebase.svg';
-import tailwind from './../images/tailwind.svg';
+import { useEffect } from 'react'
+import Linked from './../components/Linked'
+import react from './../images/react.svg'
+import html from './../images/html.svg'
+import css from './../images/css.svg'
+import javaScript from './../images/javaScript.svg'
+import firebase from './../images/firebase.svg'
+import tailwind from './../images/tailwind.svg'
 
 const About = () => {
+  useEffect(() => {
+    const script = document.createElement('script')
+
+    script.src = 'https://platform.linkedin.com/badges/js/profile.js'
+    script.async = true
+    script.defer = true
+
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
   return (
     <div className=' mt-4 mx-4 mb-4'>
       <section>
-        <h2 className='text-center text-gray-700 font-semibold text-3xl'>
+        <h2 className='text-center text-gray-700 font-semibold text-3xl m-7'>
           About
         </h2>
         <div className='flex justify-center'>
-          <img
-            src={me}
-            alt='a pic of myself'
-            className='w-2/3 h-90 sm:w-1/3 lg:w-1/4 md:w-1/3'
-          />
+          <div
+            class='badge-base LI-profile-badge'
+            data-locale='en_US'
+            data-size='large'
+            data-theme='light'
+            data-type='HORIZONTAL'
+            data-vanity='abraham-aiyedogbon-9a7095282'
+            data-version='v1'
+          ></div>
         </div>
       </section>
       <div className='sm:w-[55%] sm:mx-auto flex flex-col justify-between mt-8'>
         <section>
           <h2
             style={{ borderBottom: '1px solid #9ca3af' }}
-            className='text-gray-700 font-semibold text-lg mb-4'>
+            className='text-gray-700 font-semibold text-lg mb-4'
+          >
             Contact
           </h2>
           <p className='mb-3'>
             Hey! I post on{' '}
             <a
               href='https://twitter.com/joshuaAAbraham?s=20'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               Twitter
             </a>{' '}
             and{' '}
             <a
               href='https://linkedin.com/in/abraham-aiyedogbon-9a7095282'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               LinkedIn
             </a>{' '}
             often. Check out my amazing contents. If you want to get in touch,
@@ -49,26 +69,30 @@ const About = () => {
         <div className=' contact mt-2'>
           <h2
             style={{ borderBottom: '1px solid #9ca3af' }}
-            className='text-gray-700 font-semibold text-lg mb-4'>
+            className='text-gray-700 font-semibold text-lg mb-4'
+          >
             My Collections
           </h2>
           <p className='mb-3'>
             I write on{' '}
             <a
               href='https://hashnode.com/@AiyedogbonAbraham'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               hashnode
             </a>
             , you can follow and read my{' '}
             <a
               href='https://aiyedogbonabraham.hashnode.dev/'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               blogs
             </a>
             , connect with me on{' '}
             <a
               href='https://linkedin.com/in/abraham-aiyedogbon-9a7095282'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               LinkedIn
             </a>
             .
@@ -84,26 +108,22 @@ const About = () => {
         <section className='mt-2'>
           <h2
             style={{ borderBottom: '1px solid #9ca3af' }}
-            className='text-gray-700 font-semibold text-lg mb-4'>
+            className='text-gray-700 font-semibold text-lg mb-4'
+          >
             Skills
           </h2>
 
           <p>
             I'm an architect of{' '}
-            <span className='font-bold'>
-              user-friendly interfaces
-            </span>
-            , with a keen eye for detail and a commitment to{' '}            
-<span className='font-bold'>
-            creating interfaces
-  </span> that are both efficient and effective.
+            <span className='font-bold'>user-friendly interfaces</span>, with a
+            keen eye for detail and a commitment to{' '}
+            <span className='font-bold'>creating interfaces</span> that are both
+            efficient and effective.
           </p>
           <p className='mt-1'>
-            Here are some of the{' '}
-            <span className='font-bold'>tools</span> I have{' '}
-            <span className='font-bold'>worked</span> with over
-            the years, for my{' '}
-            <span className='font-bold'>personal</span>,{' '}
+            Here are some of the <span className='font-bold'>tools</span> I have{' '}
+            <span className='font-bold'>worked</span> with over the years, for
+            my <span className='font-bold'>personal</span>,{' '}
             <span className='font-bold'>professional</span> and{' '}
             <span className='font-bold'>open source </span>
             projects:
@@ -189,7 +209,7 @@ const About = () => {
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
