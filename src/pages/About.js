6ca31a7644b0b1a781e9643f8fe01a1a09194888
ayanfeOlugbeja <ayanfeolugbeja@gmail.com>
2024-07-05 10:59:04 +0100

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,20 @@
-import Linked from './../components/Linked';
-import me from './../images/me.png';
-import react from './../images/react.svg';
-import html from './../images/html.svg';
-import css from './../images/css.svg';
-import javaScript from './../images/javaScript.svg';
-import firebase from './../images/firebase.svg';
-import tailwind from './../images/tailwind.svg';
+import Linked from './../components/Linked'
+import me from './../images/me.png'
+import react from './../images/react.svg'
+import html from './../images/html.svg'
+import css from './../images/css.svg'
+import javaScript from './../images/javaScript.svg'
+import firebase from './../images/firebase.svg'
+import tailwind from './../images/tailwind.svg'
 
 const About = () => {
   return (
     <div className=' mt-4 mx-4 mb-4'>
       <section>
-<<<<<<< HEAD
         <h2 className='text-center text-gray-700 font-semibold text-3xl'>
           About
         </h2>
-=======
-       
->>>>>>> 52795af9cc6193ad888e21f178b0e9525ba3ebc7
+
         <div className='flex justify-center'>
           <img
             src={me}
@@ -30,20 +27,23 @@ const About = () => {
         <section>
           <h2
             style={{ borderBottom: '1px solid #9ca3af' }}
-            className='text-gray-700 font-semibold text-lg mb-4'>
+            className='text-gray-700 font-semibold text-lg mb-4'
+          >
             Contact
           </h2>
           <p className='mb-3'>
             Hey! I post on{' '}
             <a
               href='https://twitter.com/joshuaAAbraham?s=20'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               Twitter
             </a>{' '}
             and{' '}
             <a
               href='https://linkedin.com/in/abraham-aiyedogbon-9a7095282'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               LinkedIn
             </a>{' '}
             often. Check out my amazing contents. If you want to get in touch,
@@ -53,26 +53,30 @@ const About = () => {
         <div className=' contact mt-2'>
           <h2
             style={{ borderBottom: '1px solid #9ca3af' }}
-            className='text-gray-700 font-semibold text-lg mb-4'>
+            className='text-gray-700 font-semibold text-lg mb-4'
+          >
             My Collections
           </h2>
           <p className='mb-3'>
             I write on{' '}
             <a
               href='https://hashnode.com/@AiyedogbonAbraham'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               hashnode
             </a>
             , you can follow and read my{' '}
             <a
               href='https://aiyedogbonabraham.hashnode.dev/'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               blogs
             </a>
             , connect with me on{' '}
             <a
               href='https://linkedin.com/in/abraham-aiyedogbon-9a7095282'
-              className='text-red-500'>
+              className='text-red-500'
+            >
               LinkedIn
             </a>
             .
@@ -88,26 +92,22 @@ const About = () => {
         <section className='mt-2'>
           <h2
             style={{ borderBottom: '1px solid #9ca3af' }}
-            className='text-gray-700 font-semibold text-lg mb-4'>
+            className='text-gray-700 font-semibold text-lg mb-4'
+          >
             Skills
           </h2>
 
           <p>
             I'm an architect of{' '}
-            <span className='font-bold'>
-              user-friendly interfaces
-            </span>
-            , with a keen eye for detail and a commitment to{' '}            
-<span className='font-bold'>
-            creating interfaces
-  </span> that are both efficient and effective.
+            <span className='font-bold'>user-friendly interfaces</span>, with a
+            keen eye for detail and a commitment to{' '}
+            <span className='font-bold'>creating interfaces</span> that are both
+            efficient and effective.
           </p>
           <p className='mt-1'>
-            Here are some of the{' '}
-            <span className='font-bold'>tools</span> I have{' '}
-            <span className='font-bold'>worked</span> with over
-            the years, for my{' '}
-            <span className='font-bold'>personal</span>,{' '}
+            Here are some of the <span className='font-bold'>tools</span> I have{' '}
+            <span className='font-bold'>worked</span> with over the years, for
+            my <span className='font-bold'>personal</span>,{' '}
             <span className='font-bold'>professional</span> and{' '}
             <span className='font-bold'>open source </span>
             projects:
@@ -193,7 +193,7 @@ const About = () => {
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
