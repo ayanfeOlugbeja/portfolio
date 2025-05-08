f56adda23a87a399d27f689c68d0042e72ce9ec2
Aiyedogbon Abraham Ayodeji AO <ayanfeolugbeja@gmail.com>
2025-05-08 19:15:11 +0100

    Update Linked.js

--- src/components/Linked.js
+++ src/components/Linked.js
@@ -1,75 +1,57 @@
-import gitHub from './../images/github.svg';
-import whatsapp from './../images/whatsapp.svg';
-import twitter from './../images/twitter.svg';
-import link from './../images/link.svg';
+import gitHub from './../images/github.svg'
+import whatsapp from './../images/whatsapp.svg'
+import twitter from './../images/twitter.svg'
+import link from './../images/link.svg'
 
 const Linked = () => {
-  return (
-    <div className='social-links mx-auto w-full lg:w-auto mt-4 lg:mt-0 flex justify-center lg:justify-start'>
-      <ul className='flex lg:flex-col flex-row gap-4 items-center'>
-        <li>
-          <a
-            href='https://github.com/ayanfeOlugbeja'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='GitHub'
-            className='hover:scale-110 transition-transform duration-300'>
-            <img
-              src={gitHub}
-              alt='GitHub'
-              className='h-8 w-8 lg:h-6 lg:w-6'
-              loading='lazy'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://wa.me/message/GYJUYSL4SGQVH1'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='WhatsApp'
-            className='hover:scale-110 transition-transform duration-300'>
-            <img
-              src={whatsapp}
-              alt='WhatsApp'
-              className='h-8 w-8 lg:h-6 lg:w-6'
-              loading='lazy'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://twitter.com/joshuaAAbraham?s=20'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='Twitter'
-            className='hover:scale-110 transition-transform duration-300'>
-            <img
-              src={twitter}
-              alt='Twitter'
-              className='h-8 w-8 lg:h-6 lg:w-6'
-              loading='lazy'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://linkedin.com/in/aiyedogbon'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='LinkedIn'
-            className='hover:scale-110 transition-transform duration-300'>
-            <img
-              src={link}
-              alt='LinkedIn'
-              className='h-8 w-8 lg:h-6 lg:w-6'
-              loading='lazy'
-            />
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-};
+  const socialLinks = [
+    {
+      name: 'GitHub',
+      href: 'https://github.com/ayanfeOlugbeja',
+      icon: gitHub,
+    },
+    {
+      name: 'WhatsApp',
+      href: 'https://wa.me/message/GYJUYSL4SGQVH1',
+      icon: whatsapp,
+    },
+    {
+      name: 'Twitter',
+      href: 'https://twitter.com/joshuaAAbraham?s=20',
+      icon: twitter,
+    },
+    {
+      name: 'LinkedIn',
+      href: 'https://linkedin.com/in/aiyedogbon',
+      icon: link,
+    },
+  ]
 
-export default Linked;
+  return (
+    <nav className='social-links' aria-label='Social media links'>
+      <ul className='flex lg:flex-col flex-row gap-6 items-center'>
+        {socialLinks.map((social) => (
+          <li key={social.name}>
+            <a
+              href={social.href}
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label={`Visit my ${social.name} profile`}
+              className='group relative p-3 inline-flex items-center justify-center hover:bg-gray-50 rounded-full transition-all duration-300 ease-in-out'
+            >
+              <img
+                src={social.icon}
+                alt=''
+                className='h-5 w-5 opacity-75 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300 ease-in-out'
+                loading='lazy'
+              />
+              <span className='absolute -right-2 -top-2 w-2 h-2 bg-blue-600 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-300'></span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Linked
